ba8371847d2c967dcdd72d9c8445e97ef4aba8c6
kjchaps <67558731+kjchaps@users.noreply.github.com>
2025-02-26 10:49:05 +0800

    Update ex_stage.sv
    Extended FP wrap to match new inputs, added these new inputs but not yet assigned

--- core/ex_stage.sv
+++ core/ex_stage.sv
@@ -280,6 +280,10 @@ module ex_stage
   logic [CVA6Cfg.NrIssuePorts-1:0] one_cycle_select;
   assign one_cycle_select = alu_valid_i | branch_valid_i | csr_valid_i;
 
+	//sub8
+	logic[2:0] = fpu_sub8_srcfmt;
+	logic[2:0] = fpu_sub8_dstfmt;
+
   fu_data_t one_cycle_data;
   logic [CVA6Cfg.VLEN-1:0] rs1_forwarding;
   logic [CVA6Cfg.VLEN-1:0] rs2_forwarding;
@@ -444,7 +448,9 @@ module ex_stage
           .fpu_trans_id_o(fpu_trans_id),
           .result_o(fpu_result),
           .fpu_valid_o(fpu_valid),
-          .fpu_exception_o
+          .fpu_exception_o, 
+					.fpu_sub8_srcfmt_i(fpu_sub8_srcfmt),
+					.fpu_sub8_dstfmt_i(fpu_sub8_dstfmt)
       );
     end else begin : no_fpu_gen
       assign fpu_ready_o     = '0;
