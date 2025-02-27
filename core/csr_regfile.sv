72dc4c80a91159446f39d73d59420d239aea3879
kjchaps <67558731+kjchaps@users.noreply.github.com>
2025-02-27 14:39:54 +0800

    Update csr_regfile.sv

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -360,8 +360,8 @@ module csr_regfile
           if (CVA6Cfg.FpPresent && !(mstatus_q.fs == riscv::Off || (CVA6Cfg.RVH && v_q && vsstatus_q.fs == riscv::Off))) begin
             //** modified for SUB8, still have to do write access, testing 
             csr_rdata = {{CVA6Cfg.XLEN - 23{1'b0}},   //adding for XLEN size
-                          fcsr_q.seft,                // SEFT field
-                          fcsr_q.ssft,                // SSFT field
+                          fcsr_q.eft,                // SEFT field
+                          fcsr_q.sft,                // SSFT field
                           7'b0,                       // 7 zeros for padding for fprec
                           fcsr_q.frm, 
                           fcsr_q.fflags};
@@ -1051,12 +1051,10 @@ module csr_regfile
           if (CVA6Cfg.FpPresent && !(mstatus_q.fs == riscv::Off || (CVA6Cfg.RVH && v_q && vsstatus_q.fs == riscv::Off))) begin
             dirty_fp_state_csr = 1'b1;
             fcsr_d[7:0] = csr_wdata[7:0];  // ignore writes to reserved space
+						fcsr_d.eft = csr_wdata[22:19];  // SEFT (bits 19-22)
+      			fcsr_d.sft = csr_wdata[18:15];  // SSFT (bits 15–18)
             // this instruction has side-effects
             flush_o = 1'b1;
-
-      				fcsr_d.eft = csr_wdata[22:19];  // SEFT (bits 19-22)
-      				fcsr_d.sft = csr_wdata[18:15];  // SSFT (bits 15–18)
-							
           end else begin
             update_access_exception = 1'b1;
           end
